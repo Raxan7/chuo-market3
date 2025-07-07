69cbfef5d4e015de37f78c225f467f3c7422eb3b
Raxan7 <manyerere201@gmail.com>
2025-07-07 07:49:03 +0300

    Allowed utf8mb4 for emoji encoding

--- Commerce/settings.py
+++ Commerce/settings.py
@@ -125,8 +125,9 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', default='localhost'),  # Database host from .env
         'PORT': os.getenv('DB_PORT', default='3306'),  # Database port from .env
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        },
     }
 }
 
