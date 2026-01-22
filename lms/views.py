86525e239a45da23b2e13aefd672e3c84ee94904
Raxan7 <manyerere201@gmail.com>
2026-01-22 10:00:41 +0300

    Add show beta

--- lms/views.py
+++ lms/views.py
@@ -357,6 +357,18 @@ class CourseListView(ListView):
             'course_type': self.request.GET.get('course_type', ''),
             'q': self.request.GET.get('q', ''),
         }
+        context['ad_links'] = [
+            'https://www.effectivegatecpm.com/jp604fzw?key=0ac38e5ac8cfdc4296fa7f9060dcb6aa',
+            'https://www.effectivegatecpm.com/sqf24i6k9?key=69bd5c9b5bc46a7bf95c51f8a22d72be',
+            'https://www.effectivegatecpm.com/ghvacaxk?key=152c6ad3137dfd992107cafb20ba5475',
+            'https://www.effectivegatecpm.com/y56j06mc56?key=8a8851afe3dc03e0d7ad3a66474de43d',
+            'https://www.effectivegatecpm.com/cdrnmt82a0?key=f324af40210b539b89a9b3648da6d1a6',
+            'https://www.effectivegatecpm.com/x127x1tt?key=057c2839b78ce9b3e3d4222043664ed1',
+            'https://www.effectivegatecpm.com/t8969tvrft?key=a7ad665ecba681e29abdc931553dded0',
+            'https://www.effectivegatecpm.com/ue2x9peh?key=edf04c7d5e5bf35cb005016b6dc7e7c7',
+            'https://www.effectivegatecpm.com/bh2zyhrc?key=1e19b0229c99212775e63f84220c3f4d',
+            'https://www.effectivegatecpm.com/wz236iiy?key=ea56dcc4df8cdce94fbd30f67f6e6b0d',
+        ]
         return context
 
 
