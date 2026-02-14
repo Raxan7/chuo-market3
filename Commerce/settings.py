d4ad363e7adf8d3682035ed956152d6f3bb2ebd4
Raxan7 <manyerere201@gmail.com>
2026-02-14 13:30:26 +0300

    Turned on debug

--- Commerce/settings.py
+++ Commerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Define the canonical domain (without www)
 CANONICAL_DOMAIN = 'chuosmart.com'
