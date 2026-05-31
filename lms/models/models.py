f89a38114390b96af5048047cb58b3d945c6913e
Raxan7 <manyerere201@gmail.com>
2026-05-31 15:50:50 +0300

    Defined timezone

--- lms/models/models.py
+++ lms/models/models.py
@@ -10,6 +10,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from django.utils import timezone
 from datetime import timedelta
 import random
 import string
