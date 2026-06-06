1925c10810e8ebf6fc55bb30d352dfb35a881ecd
Raxan7 <manyerere201@gmail.com>
2026-06-06 14:24:01 +0300

    fixing imports

--- lms/models/__init__.py
+++ lms/models/__init__.py
@@ -1,27 +1,6 @@
-"""
-Init file for lms.models package
-"""
-
-# Import all constants from models.py
-from .models import (
-    LEVEL_CHOICES, SEMESTER_CHOICES, YEARS, CATEGORY_OPTIONS,
-    CHOICE_ORDER_OPTIONS, ESSAY_ANSWER_TYPES
-)
-
-# Import all models from models.py
-from .models import (
-    ActivityLog, Semester, LMSProfile, Program, Course, CourseEnrollment, 
-    CourseModule, CourseContent, ContentAccess, Quiz, Question, MCQuestion,
-    Choice, TF_Question, Essay_Question, QuizTaker, StudentAnswer, Grade,
-    InstructorRequest, PaymentMethod, ModuleProgress, CertificateTemplate,
-    StudentCertificate
-)
-
-# Import utility functions from models.py
-from .models import unique_slug_generator
-
-# Import SiteSettings from site_settings.py
+from .models import ActivityLog, Semester, LMSProfile, Program, Course, CourseEnrollment, \
+    CourseModule, CourseContent, ContentAccess, Quiz, Question, MCQuestion, Choice, \
+    TF_Question, Essay_Question, QuizTaker, StudentAnswer, Grade, InstructorRequest, \
+    PaymentMethod, ModuleProgress, CertificateTemplate, StudentCertificate, QuizGenerationJob
 from .site_settings import SiteSettings
-
-# Import AdExemptUser from ad_exempt.py
 from .ad_exempt import AdExemptUser
